6c7a6c7f23e458b8897c1da1df28eb091f8c5e6b
Bikash Kakati <bikashkakati796@gmail.com>
2024-01-17 23:16:41 +0530

    Fix Bug- Added env

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -1,17 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBTnK9n-q47weZjRzj8Ri4lhuBOuR5OksE",
-  authDomain: "mail-box-b90cd.firebaseapp.com",
-  projectId: "mail-box-b90cd",
-  storageBucket: "mail-box-b90cd.appspot.com",
-  messagingSenderId: "455259245021",
-  appId: "1:455259245021:web:b530c664b78599a583ce34"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
