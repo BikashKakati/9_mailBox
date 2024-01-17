d181e699afeaeabb1eb4ea6b3476e4121452b1b5
Bikash Kakati <bikashkakati796@gmail.com>
2024-01-17 23:25:03 +0530

    Style- Change some style

--- src/components/ui/MailList.jsx
+++ src/components/ui/MailList.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const MailList = () => {
+    const mail = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam est adipisci fugiat dicta, ipsam ex. Dolorem animi reiciendis deleniti illum maiores repellat earum, rerum veritatis minima placeat natus ducimus quidem."
     return (
         <tr className="border-y-1 border-zinc-300">
             <td>randeep@gamil.com</td>
-            <td>Quality Control Specialist</td>
-            <td>Quality Control Specialist</td>
+            <td className='font-semibold'>Meeting scheduled</td>
+            <td>{mail.slice(0,25)+"..."}</td>
+            <td>12 aug 2022</td>
             <td>
                 <button className='btn btn-primary'>Delete</button>
             </td>
