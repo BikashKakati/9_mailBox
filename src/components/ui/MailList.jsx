ed90268de3957a2a7fb203f0acbba104485d0aa0
Bikash Kakati <bikashkakati796@gmail.com>
2024-01-18 22:11:01 +0530

    Feature - Added unread mail count

--- src/components/ui/MailList.jsx
+++ src/components/ui/MailList.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { recievedMailHandler } from '../../services/redux/api/mailsThunk';
+import { toast } from 'react-hot-toast';
 
-const MailList = () => {
-    const mail = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam est adipisci fugiat dicta, ipsam ex. Dolorem animi reiciendis deleniti illum maiores repellat earum, rerum veritatis minima placeat natus ducimus quidem."
+const MailList = ({mailDetails, type}) => {
+    const {sender, message, subject,id, read} = mailDetails;
+    const dispatch = useDispatch();
+    const Navigate = useNavigate();
+    // const date = new Date(timeStamp.seconds*1000).toLocalTimeString();
+    async function handleMailOpening(){
+        toast.loading("Loading...");
+        if(!read){
+            await dispatch(recievedMailHandler({...mailDetails, read:true}));
+        }
+        Navigate(`/details/${type}/${id}`);
+        toast.remove();
+    }
     return (
-        <tr className="border-y-1 border-zinc-300">
-            <td>randeep@gamil.com</td>
-            <td className='font-semibold'>Meeting scheduled</td>
-            <td>{mail.slice(0,25)+"..."}</td>
+        <tr className="border-y-1 border-zinc-300 cursor-pointer" onClick={handleMailOpening}>
+            <td>{!read && "🔵 "}{"  "}{sender}</td>
+            <td className='font-semibold'>{subject}</td>
+            <td>{message?.slice(0,25)+"..."}</td>
             <td>12 aug 2022</td>
             <td>
                 <button className='btn btn-primary'>Delete</button>
--- src/services/redux/api/mailsThunk.js
+++ src/services/redux/api/mailsThunk.js
@@ -1,28 +1,43 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { doc, setDoc } from "firebase/firestore";
+import { collection, doc, getDocs, onSnapshot, setDoc } from "firebase/firestore";
 import { db } from "../../firebase-config";
 
 export const sentMailHandler = createAsyncThunk(
     "mail/sentMail",
-    async function (mailDetails,{getState}){
-        const email = getState().auth.currentUser?.email;
-        try{
-            const docRef = doc(db,`sentMails/${email}/mails/${mailDetails.id}`);
-            await setDoc(docRef, {...mailDetails, read:true});
+    async function (mailDetails, { getState }) {
+        const userEmail = getState().auth.currentUser?.email;
+        try {
+            const docRef = doc(db, `sentMails/${userEmail}/mails/${mailDetails.id}`);
+            await setDoc(docRef, { ...mailDetails, read: true});
             return mailDetails;
-        }catch(err){
+        } catch (err) {
             throw new Error(err);
         }
     }
 )
 export const recievedMailHandler = createAsyncThunk(
     "mail/recievedMail",
-    async function (mailDetails){
-        try{
-            const docRef = doc(db,`recievedMail/${mailDetails.mail}/mails/${mailDetails.id}`);
-            await setDoc(docRef, {...mailDetails, read:false});
-        }catch(err){
+    async function (mailDetails) {
+        try {
+            const docRef = doc(db, `recievedMail/${mailDetails.to}/mails/${mailDetails.id}`);
+            await setDoc(docRef, { ...mailDetails, read: mailDetails.read?true:false });
+        } catch (err) {
             throw new Error(err);
+        }
+    }
+)
+export const getSentMails = createAsyncThunk(
+    "expense/getExpenseData",
+    async function (_, { getState }) {
+        let details = [];
+        const userEmail = getState()?.auth?.currentUser?.email;
+        try {
+            const docRef = collection(db, `sentMails/${userEmail}/mails`);
+            const storageData = await getDocs(docRef);
+            storageData?.forEach(doc => {details.push(doc?.data())})
+            return details;
+        } catch (err) {
+            throw new Error(err.message);
         }
     }
 )
