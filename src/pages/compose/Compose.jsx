dfd27a27f489dad81d69cbe8a22a5dc118bb8966
Bikash Kakati <bikashkakati796@gmail.com>
2024-01-18 22:12:26 +0530

    Feature- Mail compose completed
    Mistakenly forgot to push with mail thunk

--- src/pages/compose/Compose.jsx
+++ src/pages/compose/Compose.jsx
@@ -1,17 +1,49 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Wrapper from '../../components/ui/Wrapper'
+import { toast } from 'react-hot-toast';
+import { useDispatch, useSelector } from 'react-redux';
+import { recievedMailHandler, sentMailHandler } from '../../services/redux/api/mailsThunk';
+import { Firestore } from 'firebase/firestore';
 
 const Compose = () => {
+  const mailRef = useRef();
+  const subjectRef = useRef();
+  const messageRef = useRef();
+  const dispatch = useDispatch();
+  const {currentUser} = useSelector(state => state.auth);
+
+  async function handleMailSubmit(e) {
+    e.preventDefault();
+    const mail = mailRef.current.value;
+    const subject = subjectRef.current.value;
+    const message = messageRef.current.value;
+    if (!mail.length) {
+      toast.error("Please specify at least one recipient.");
+      return;
+    }
+    const mailDetails = {
+      id: "user" + String(Date.now()),
+      sender: currentUser.email,
+      to:mail,
+      subject,
+      message,
+    }
+    await dispatch(sentMailHandler(mailDetails));
+    await dispatch(recievedMailHandler(mailDetails));
+    toast.remove();
+    toast.success("email sent successfully!");
+    e.target.reset();
+  }
 
   return (
     <Wrapper>
-      <div className="max-w-[35rem] mx-auto bg-white">
+      <div className="max-w-[35rem] mx-auto bg-white shadow-md">
         <p className="px-5 py-3 bg-green-100">New Message</p>
-        <form className='px-4 pb-4'>
-        <input type="email" className="w-full pt-4  border-b-[1px] border-zinc-300 outline-none" placeholder='To' />
-        <input type="text" className="w-full pt-4 border-b-[1px] border-zinc-300 outline-none" placeholder='Subject'/>
-        <textarea rows="10" className='w-full pt-4 outline-none resize-none'/>
-        <button type='submit' className='btn btn-info w-24'>Send</button>
+        <form className='px-4 pb-4' onSubmit={handleMailSubmit}>
+          <input type="email" className="w-full pt-4  border-b-[1px] border-zinc-300 outline-none" placeholder='To' ref={mailRef} />
+          <input type="text" className="w-full pt-4 border-b-[1px] border-zinc-300 outline-none" placeholder='Subject' ref={subjectRef} />
+          <textarea rows="10" className='w-full pt-4 outline-none resize-none' placeholder='Write message' ref={messageRef} />
+          <button type='submit' className='btn btn-info w-24'>Send</button>
         </form>
       </div>
     </Wrapper>
