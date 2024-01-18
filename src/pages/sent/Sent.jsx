2cf4bd48d161cf6abc742dd24608c5ad86b65462
Bikash Kakati <bikashkakati796@gmail.com>
2024-01-18 22:13:21 +0530

    Feature - Sent message
    Now sent messages showing on sent section

--- src/pages/sent/Sent.jsx
+++ src/pages/sent/Sent.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Wrapper from '../../components/ui/Wrapper';
+import MailTable from '../../components/ui/MailTable';
+import MailList from '../../components/ui/MailList';
+import { getSentMails } from '../../services/redux/api/mailsThunk';
 
 const Sent = () => {
+  const {sentMails} = useSelector(state => state.mail);
+  const {currentUser} = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+
   return (
-    <div>Sent</div>
+    <Wrapper>
+      <MailTable>
+        {
+          !!sentMails.length &&
+          sentMails.map(mail => {
+            return <MailList key={mail.id} mailDetails={mail} type="sent"/>
+          })
+        }
+      </MailTable>
+    </Wrapper>
   )
 }
 
