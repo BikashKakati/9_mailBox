286fc1a7b77a496f179c98f9e41e195ca86cdc0e
Bikash Kakati <bikashkakati796@gmail.com>
2024-01-19 23:09:01 +0530

    Feature - Created mail detail

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import Wrapper from '../../components/ui/Wrapper'
+import {useSelector} from "react-redux"
+import { useParams } from 'react-router-dom';
 
 const Details = () => {
+   const {sentMails, recievedMails} = useSelector(state => state.mail);
+   const {type, id:currentMailId} = useParams();
+   const mailType = type === "recievedMails" ? recievedMails : sentMails;
+   const mailDetail = mailType.find(mail => mail.id === currentMailId);
     return (
         <Wrapper>
-            <div className="bg-white py-5 px-16">
-                <p className="pb-8 text-2xl">Looking for an opportunity</p>
-                <div className="font-semibold pb-6 flex justify-between">
-                    <span>test@gam.com</span>
-                    <span className="">12 sept</span>
+            <div className="bg-white pt-8 pb-12 px-16">
+                <p className="pb-4 text-2xl capitalize border-b-[1px]">{mailDetail?.subject}</p>
+                <div className="font-semibold py-6 flex justify-between border-b-[1px]">
+                    <span>{mailDetail?.sender}</span>
+                    <span className="">{mailDetail?.timeStamp}</span>
                 </div>
-                <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet corporis consectetur aspernatur animi deleniti delectus debitis est sint nemo, nisi numquam voluptates neque ex perferendis sed? Deleniti asperiores ut vitae?</p>
+                <pre className="pt-4 text-sm text-wrap">
+                    {mailDetail?.message}
+                </pre>
             </div>
         </Wrapper>
     )
