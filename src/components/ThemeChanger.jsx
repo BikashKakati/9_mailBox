5ffb0bee8c8acb26befe0c5c743660fa57a8598a
Bikash Kakati <bikashkakati796@gmail.com>
2024-03-11 19:59:34 +0530

    Fix bug- theme logo fixed

--- src/components/ThemeChanger.jsx
+++ src/components/ThemeChanger.jsx
@@ -4,9 +4,9 @@ export const ThemeChanger = ({ onThemeChange, theme }) => {
     return <button className='bg-ghost mr-10' onClick={onThemeChange}>
         {
             theme === "light" ?
-                <SunIcon className="fill-current w-8 h-8" />
-                :
                 <MoonIcon className="fill-current w-8 h-8" />
+                :
+                <SunIcon className="fill-current w-8 h-8" />
         }
     </button>
 }
